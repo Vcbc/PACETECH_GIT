25ef5f024f38435d997cc3e2dabbc8a4e4da3618
Victor Cézar Bonatti <victorcezarbonatti@hotmail.com>
2022-08-21 09:33:07 -0300

    meu primeiro commit

---
+++ src/pacetech/git/PACETECHGIT.java
@@ -0,0 +1,15 @@
+
+package pacetech.git;
+
+public class PACETECHGIT {
+
+  
+    public static void main(String[] args) {
+        
+        int aa = 10;
+        
+        System.out.println(aa);
+     
+    }
+    
+}
